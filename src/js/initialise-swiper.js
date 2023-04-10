fb663716af10f21b3c593fa5bd3c492fb376c7a1
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2023-04-10 08:47:32 +0300

    components & products
    небольшие правки

--- src/js/initialise-swiper.js
+++ src/js/initialise-swiper.js
@@ -2,20 +2,13 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 18,
+  
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
